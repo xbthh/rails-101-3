d5d9f50ebcc5e436dac60d7f728bc4e0d605acd8
xbthh <hanhan_smith@hotmail.com>
2016-10-09 22:02:16 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,14 @@ class User < ApplicationRecord
     participated_groups.include?(group)
   end
 
+  def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
+  end
+
   has_many :groups
   has_many :posts
 
